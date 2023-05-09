49856a4fe9eb70d875592727e3d44001444f13a2
Jeff Hemmen <jeff@hemmen.lu>
2023-05-09 16:57:26 +0200

    Collapsible tree

--- js/main.js
+++ js/main.js
@@ -1,83 +1,40 @@
-// document.addEventListener('DOMContentLoaded', () => {
-//     const inputBox = document.getElementById('input-box');
-//     const outputBox = document.getElementById('output-box');
-
-    
-//     function showError(message) {
-//         const errorMessageContainer = document.getElementById('error-message-container');
-//         errorMessageContainer.innerHTML = `<div id="error-message">${message}</div>`;
-//     }
-
-//     function parse_policy(ptree, policyText) {
-//         const regex = /path\s*"(.*)"\s*{\s*capabilities\s*=\s*(\[\s*".*"\s*\])\s*}/g;
-//         let match;
-//         while ((match = regex.exec(policyText)) !== null) {
-//             const path = match[1];
-//             const capabilityList = JSON.parse(match[2]);
-//             const capabilities = new Set(capabilityList);
-//             ptree.add_path(path, capabilities);
-//         }
-//     }
-
-//     // Instantiate PolicyTree
-//     // PolicyTree = new PolicyTree();
-
-//     inputBox.addEventListener('input', () => {
-//         try {
-//             const pt = new PolicyTree();
-
-//             // Build the tree by calling add_path with the input in the left box
-//             const policyText = inputBox.value;
-//             parse_policy(pt, policyText);
-
-//             // Populate the right box with the evaluated examples
-//             let result = '';
-//             for (const example of pt.examples()) {
-//                 result += `${example} ${pt.evaluate(example)}\n`;
-//             }
-//             outputBox.value = result;
-
-//             // Remove error message if present
-//             if (document.getElementById('error-message')) {
-//                 document.getElementById('error-message').remove();
-//             }
-//         } catch (error) {
-//             // Display error message
-//             showError('Invalid input');
-//         }
-//     });
-// });
-
-
 document.addEventListener('DOMContentLoaded', () => {
     const inputBox = document.getElementById('input-box');
     const outputBox = document.getElementById('output-box');
     const errorBubble = document.getElementById('error-message-container');
     const lightDarkToggle = document.getElementById('theme-toggle');
-    const feedbackBtn = document.getElementById("feedback-button");
+    const feedbackBtn = document.getElementById('feedback-button');
+    const feedbackOptions = document.getElementById("feedback-options");
+    const examplesCheckbox = document.getElementById('auto-generate');
+    const examplesTextbox = document.getElementById('example-paths');
+    const outputTree = document.getElementById('output-tree')
 
 
-    const processInput = () => {
+    let pt;
+    let examples;
+
+
+/* RIGHT-HAND SIDE */
+    function gen_output() {
+        outputBox.value = examples.join('\n');
+        outputTree.innerHTML = createTree(examples);
+        console.log(examples);
+    }
+
+/* INPUT */
+
+    function processInput() {
         const input = inputBox.value;
         if (!validate_input(input)) {
             errorBubble.classList.remove('hidden');
         } else {
             errorBubble.classList.add('hidden');
-            const pt = new PolicyTree();
+            pt = new PolicyTree();
             parse_policy(pt, input);
-            let output = "";
-            for (let p of pt.examples()) {
-                output += p + " " + pt.evaluate(p) + "\n";
-            }
-            outputBox.value = output;
+            generateExamples();
+
         }
-    };
-
-    inputBox.addEventListener('input', processInput);
-
-    lightDarkToggle.addEventListener('click', () => {
-        document.body.classList.toggle('dark-mode');
-    });
+    }
 
     function parse_policy(ptree, policyText) {
         const regex = /path\s*"(.*)"\s*{\s*capabilities\s*=\s*(\[\s*".*"\s*\])\s*}/g;
@@ -94,11 +51,106 @@ document.addEventListener('DOMContentLoaded', () => {
         return true;
     }
 
-    const feedbackOptions = document.getElementById("feedback-options");
+/* OUTPUT TREE */
+
+    function outputTreeNode(path) {
+        this.path = path;
+        this.children = new Array();
+    }
+
+    function createTree(paths) {
+
+        paths.sort((a, b) => a.length - b.length);
+        const index_map = new Map();    // maps strings to outputTreeNode instances
+        const no_parents = [];          // list of outputTreeNode instances
+
+        paths.forEach(path => {
+            let has_parent = false;
+            let longest_parent = "";
+            for (const parent of index_map.keys()) {
+                if (path.includes(parent)) {
+                    has_parent = true;
+                    if (parent.length > longest_parent.length) {
+                        longest_parent = parent;
+                    }
+                }
+            }
+            let new_node = new outputTreeNode(path);
+            index_map.set(path, new_node);
+            if (!has_parent) {
+                no_parents.push(new_node);
+            } else {
+                let parent_node = index_map.get(longest_parent);
+                parent_node.children.push(new_node);
+            }
+        });
+
+
+        console.log(no_parents);
+
+        function generateHtml(node, depth = 0) {
+            let html = '<li>';
+            if (node.children.length == 0) {
+                html += node.path;
+            } else {
+                html += '<details open>\n<summary>' + node.path + '</summary>\n';
+            
+                html += '<ul>';
+                node.children.forEach(child => {
+                    html += generateHtml(child, depth + 1);
+                });
+                html += '</ul>\n</details>';
+            }
+            html += '</li>\n';
+            return html;
+        }
+
+
+        let whole_tree_html = "<ul>";
+        no_parents.forEach(node => {
+            whole_tree_html += generateHtml(node);
+        });
+        whole_tree_html += '</ul>';
+        console.log(whole_tree_html);
+        return whole_tree_html;
+
+    }
+
+
+/* EXAMPLES*/
+    
+    function generateExamples() {
+        if (examplesCheckbox.checked) {
+            examplesTextbox.setAttribute('readonly', '');
+            examples = pt.examples()
+            examplesTextbox.value = examples.join('\n');
+        } else {
+            examplesTextbox.removeAttribute('readonly');
+            examples = examplesTextbox.value.split('\n');
+        }
+
+        gen_output();
+    }
+
+
+
+
+
+/* EVENT LISTENERS */
+
+    inputBox.addEventListener('input', processInput);
+
+    lightDarkToggle.addEventListener('click', () => {
+        document.body.classList.toggle('dark-mode');
+    });
+
 
     feedbackBtn.addEventListener("click", function () {
         feedbackOptions.classList.toggle("hidden");
     });
     
+    examplesCheckbox.addEventListener('change', generateExamples);
+    examplesTextbox.addEventListener('input', generateExamples);
+    
 
-});
+    });
